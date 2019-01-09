6362f8c1a581f45e6ba999c45989f3b9616b13f2
Gabe Fierro <gtfierro225@gmail.com>
2019-01-08 20:14:26 -0800

    add sample query to the "main" method for testing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	mortarpb "git.sr.ht/~gabe/mortar/proto"
 	"log"
 	"time"
 )
@@ -10,11 +11,34 @@ func main() {
 
 	maincontext, cancel := context.WithCancel(context.Background())
 
-	loadgen_stage := NewSimpleLoadGenStage()
+	// do query with uuid: 5bd3a840-6ee6-5922-aeaf-2d7ec0bb4cff(
+	req := mortarpb.FetchRequest{
+		Sites: []string{"test_me_site_name"},
+		Streams: []*mortarpb.Stream{
+			{
+				Name:        "test1",
+				Definition:  "",
+				Uuids:       []string{"5bd3a840-6ee6-5922-aeaf-2d7ec0bb4cff"},
+				Aggregation: mortarpb.AggFunc_AGG_FUNC_RAW,
+				Units:       "",
+			},
+		},
+		Time: &mortarpb.TimeParams{
+			Start: "2019-01-07T00:00:00Z",
+			End:   "2019-01-10T00:00:00Z",
+		},
+	}
+	ctx1 := Context{
+		ctx:     context.Background(),
+		request: req,
+	}
+
+	loadgen_stage := NewSimpleLoadGenStage(ctx1)
 
 	ts_stage_cfg := &TimeseriesStageConfig{
 		Upstream:     loadgen_stage,
 		StageContext: maincontext,
+		BTrDBAddress: "127.0.0.1:4410",
 	}
 	ts_stage, err := NewTimeseriesQueryStage(ts_stage_cfg)
 	if err != nil {
@@ -27,6 +51,12 @@ func main() {
 		end = end.GetUpstream()
 	}
 
-	time.Sleep(4 * time.Second)
+	log.Println("get output")
+	for out := range ts_stage.GetQueue() {
+		//log.Println(">", len(out.response.Times))
+		_ = out
+	}
+
+	time.Sleep(30 * time.Second)
 	cancel()
 }
