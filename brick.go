753ed3038c38ba845e32afbe0bd7bbb38f15b649
Gabe Fierro <gtfierro225@gmail.com>
2019-01-16 15:52:46 -0800

    Changes to brick stage
    - expose results of query to user (and send them!)
    - bootstrap the cursors using a broader query

--- brick.go
+++ brick.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"git.sr.ht/~gabe/hod/hod"
 	logpb "git.sr.ht/~gabe/hod/proto"
+	mortarpb "git.sr.ht/~gabe/mortar/proto"
 	"github.com/pkg/errors"
 	logrus "github.com/sirupsen/logrus"
 	"os"
@@ -59,7 +60,7 @@ func NewBrickQueryStage(cfg *BrickQueryStageConfig) (*BrickQueryStage, error) {
 	}
 
 	stage.highwatermark = time.Now().UnixNano()
-	q := "SELECT ?vav FROM ciee WHERE { ?vav rdf:type brick:Zone_Temperature_Sensor };"
+	q := "SELECT ?c FROM * WHERE { ?c rdf:type brick:Class };"
 	query, err := stage.db.ParseQuery(q, stage.highwatermark)
 	if err != nil {
 		return nil, err
@@ -131,6 +132,7 @@ func (stage *BrickQueryStage) processQuery(ctx Context) error {
 		// property is how to relate the points to the timeseries database. However, it also introduces the complexity
 		// of dealing with whether or not the variables *do* have associated timeseries or not.
 		startIdx := rewriteQuery(reqstream.DataVars, query)
+		query.Graphs = ctx.request.Sites
 		res, err := stage.db.Select(ctx.ctx, query)
 		if err != nil {
 			ctx.addError(err)
@@ -142,11 +144,19 @@ func (stage *BrickQueryStage) processQuery(ctx Context) error {
 		// the rewriteQuery method has to return the index that we start with when iterating through the variables in
 		// each row to make sure we get the actual queries.
 		stream := ctx.request.Streams[idx]
+
+		brickresp := &mortarpb.FetchResponse{}
+
 		for _, row := range res.Rows {
 			for uuidx := startIdx; uuidx < len(query.Vars); uuidx++ {
 				stream.Uuids = append(stream.Uuids, row.Values[uuidx].Value)
 			}
+			// we also add the query results to the output
+			brickresp.Rows = append(brickresp.Rows, transformRow(row))
 		}
+		// send the query results to the client
+		// TODO: make this streaming?
+		ctx.done <- brickresp
 
 		stage.output <- ctx
 	}
@@ -169,4 +179,12 @@ func rewriteQuery(datavars []string, query *logpb.SelectQuery) int {
 	query.Where = append(query.Where, newtriples...)
 	query.Vars = append(query.Vars, newselect...)
 	return oldidx
+}
+
+func transformRow(r *logpb.Row) *mortarpb.Row {
+	newr := &mortarpb.Row{}
+	for _, rr := range r.Values {
+		newr.Values = append(newr.Values, &mortarpb.URI{Namespace: rr.Namespace, Value: rr.Value})
+	}
+	return newr
 }
