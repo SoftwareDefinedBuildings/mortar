d2e167f385afd2d197571734e7dad8ed2a996fbc
Gabe Fierro <gtfierro225@gmail.com>
2019-01-30 20:04:54 -0800

    respect USE_HTTPS, push to :latest

--- containers/pymortar-client/start.sh
+++ containers/pymortar-client/start.sh
@@ -2,6 +2,9 @@
 export NB_USER=jovyan
 export CHOWN_HOME=yes
 export GRANT_SUDO=yes
-export USE_HTTPS=yes
 export JUPYTER_ENABLE_LAB=yes
-start-notebook.sh --NotebookApp.ip=0.0.0.0 --NotebookApp.certfile=/certs/mortar-client.pem --NotebookApp.keyfile=/certs/mortar-client.key
+if [ $USE_HTTPS = "yes" ]; then
+	start-notebook.sh --NotebookApp.ip=0.0.0.0 --NotebookApp.certfile=/certs/mortar-client.pem --NotebookApp.keyfile=/certs/mortar-client.key
+else
+	start-notebook.sh --NotebookApp.ip=0.0.0.0
+fi
