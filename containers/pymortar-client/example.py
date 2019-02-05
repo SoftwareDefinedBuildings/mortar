1c81fd337b099874f6461efa113fde4a39bf1480
Gabe Fierro <gtfierro225@gmail.com>
2019-02-05 09:27:50 -0800

    update example.py and requirements.txt in pymortar container

--- containers/pymortar-client/example.py
+++ containers/pymortar-client/example.py
@@ -7,20 +7,30 @@ resp = client.qualify([
 ])
 
 req = pymortar.FetchRequest(
-    sites=list(resp.sites)[:10],
-    streams=[
-        pymortar.Stream(
-            name="meter",
-            definition="SELECT ?meter WHERE { ?meter rdf:type/rdfs:subClassOf* brick:Electric_Meter };",
+    sites=resp.sites,
+    collections=[
+      pymortar.Collection(
+        sites=resp.sites,
+        name="meter",
+        definition="SELECT ?meter WHERE { ?meter rdf:type/rdfs:subClassOf* brick:Building_Electric_Meter };",
+      )
+    ],
+    selections=[
+      pymortar.Selection(
+        name="meter_data",
+        aggregation=pymortar.MEAN,
+        window="1h",
+        timeseries=[
+          pymortar.Timeseries(
+            collection="meter",
             dataVars=["?meter"],
-            aggregation=pymortar.MEAN,
-            units="",
-        ),
+          )
+        ],
+      )
     ],
     time=pymortar.TimeParams(
         start="2017-01-01T00:00:00Z",
         end="2018-01-01T00:00:00Z",
-        window="1h",
     )
 )
 
