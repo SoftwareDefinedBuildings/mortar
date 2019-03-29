0f6a20cdfc88cae9a5f689d1c2443e7c1740a46b
Gabe Fierro <gtfierro225@gmail.com>
2019-03-29 14:34:39 -0700

    add influxdb implementation

---
+++ stages/influxdb.go
@@ -0,0 +1,232 @@
+package stages
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	mortarpb "git.sr.ht/~gabe/mortar/proto"
+	influx "github.com/influxdata/influxdb/client/v2"
+	//"github.com/pborman/uuid"
+	"github.com/pkg/errors"
+)
+
+type InfluxDBTimeseriesQueryStage struct {
+	upstream Stage
+	ctx      context.Context
+	output   chan Context
+
+	conn influx.Client
+	// timeseries database stuff
+	//conn        *btrdb.BTrDB
+	//streamCache sync.Map
+
+	sync.Mutex
+}
+
+type InfluxDBTimeseriesStageConfig struct {
+	Upstream     Stage
+	StageContext context.Context
+	Username     string
+	Password     string
+	Address      string
+}
+
+func NewInfluxDBTimeseriesQueryStage(cfg *InfluxDBTimeseriesStageConfig) (*InfluxDBTimeseriesQueryStage, error) {
+
+	conn, err := influx.NewHTTPClient(influx.HTTPConfig{
+		Addr:     cfg.Address,
+		Username: cfg.Username,
+		Password: cfg.Password,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect influx")
+	}
+	log.Info("Connected to InfluxDB!")
+
+	stage := &InfluxDBTimeseriesQueryStage{
+		upstream: cfg.Upstream,
+		output:   make(chan Context),
+		ctx:      cfg.StageContext,
+		conn:     conn,
+	}
+
+	// TODO: configure concurrent connections
+	num_workers := 20
+	// consume function
+	for i := 0; i < num_workers; i++ {
+		go func() {
+			input := stage.upstream.GetQueue()
+			for {
+				select {
+				case ctx := <-input:
+					if len(ctx.request.Sites) > 0 && len(ctx.request.DataFrames) > 0 {
+						if err := stage.processQuery(ctx); err != nil {
+							log.Println(err)
+						}
+					}
+					ctx.response = nil
+					stage.output <- ctx
+					//ctx.done <- nil
+				case <-stage.ctx.Done():
+					// case that breaks the stage and releases resources
+					fmt.Println("Ending Timeseries Queue")
+					return
+				}
+			}
+		}()
+	}
+
+	return stage, nil
+
+}
+
+func (stage *InfluxDBTimeseriesQueryStage) GetUpstream() Stage {
+	stage.Lock()
+	defer stage.Unlock()
+	return stage.upstream
+}
+
+func (stage *InfluxDBTimeseriesQueryStage) SetUpstream(upstream Stage) {
+	stage.Lock()
+	defer stage.Unlock()
+	if stage != nil {
+		stage.upstream = upstream
+	}
+	fmt.Println("Updated stage to ", upstream)
+}
+
+func (stage *InfluxDBTimeseriesQueryStage) GetQueue() chan Context {
+	return stage.output
+}
+
+func (stage *InfluxDBTimeseriesQueryStage) String() string {
+	return "<|influx ts stage|>"
+}
+
+func (stage *InfluxDBTimeseriesQueryStage) processQuery(ctx Context) error {
+	//	defer ctx.finish()
+	fmt.Printf("INFLUX %+v\n", ctx.request)
+	// parse timestamps for the query
+	start_time, err := time.Parse(time.RFC3339, ctx.request.Time.Start)
+	if err != nil {
+		err = errors.Wrapf(err, "Could not parse Start time (%s)", ctx.request.Time.Start)
+		ctx.addError(err)
+		return err
+	}
+	end_time, err := time.Parse(time.RFC3339, ctx.request.Time.End)
+	if err != nil {
+		err = errors.Wrapf(err, "Could not parse End time (%s)", ctx.request.Time.End)
+		ctx.addError(err)
+		return err
+	}
+
+	// TODO: there is an awkward design artifact we have inherited. Essentially,
+	// we put each collection from a plugin in its own "measurement". But, we want to query
+	// by UUID, and we don't know the measurement. Either need to build some index of UUID
+	// to measurement, or just insert into a single measurement that we know from the beginning
+	for _, dataFrame := range ctx.request.DataFrames {
+		for _, uuStr := range dataFrame.Uuids {
+
+			// default for RAW
+			selector := "value"
+			var groupby string
+			if dataFrame.Aggregation != mortarpb.AggFunc_AGG_FUNC_RAW {
+				window, err := ParseDuration(dataFrame.Window)
+				if err != nil {
+					ctx.addError(err)
+					return err
+				}
+				groupby = fmt.Sprintf("GROUP BY time(%s)", window)
+			}
+
+			// TODO: this is automatically interpolating?
+
+			switch dataFrame.Aggregation {
+			case mortarpb.AggFunc_AGG_FUNC_MEAN:
+				selector = `mean("value")`
+			case mortarpb.AggFunc_AGG_FUNC_MIN:
+				selector = `min("value")`
+			case mortarpb.AggFunc_AGG_FUNC_MAX:
+				selector = `max("value")`
+			case mortarpb.AggFunc_AGG_FUNC_SUM:
+				selector = `sum("value")`
+			case mortarpb.AggFunc_AGG_FUNC_COUNT:
+				selector = `count("value")`
+			}
+
+			q_str := fmt.Sprintf(`SELECT time, %s
+                             FROM "timeseries"
+                             WHERE uuid='%s' 
+                               AND time > %d 
+                               AND time < %d
+                             %s
+                             ;`, selector, uuStr, start_time.UnixNano(), end_time.UnixNano(), groupby)
+			q := influx.Query{
+				Command:   q_str,
+				Database:  "xbos",
+				Precision: "ns",
+			}
+			resp, err := stage.conn.Query(q)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			if len(resp.Results) < 1 {
+				log.Error("nil results")
+				continue
+			}
+			if len(resp.Results[0].Series) < 1 {
+				log.Error("nil series")
+				continue
+			}
+
+			tsresp := &mortarpb.FetchResponse{}
+			var pcount = 0
+
+			for _, ser := range resp.Results[0].Series {
+				for _, row := range ser.Values {
+					if row[1] == nil {
+						continue
+					}
+
+					fmt.Println(row)
+					time, err := row[0].(json.Number).Int64()
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+					value, err := row[1].(json.Number).Float64()
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+					pcount += 1
+					tsresp.Times = append(tsresp.Times, time)
+					tsresp.Values = append(tsresp.Values, value)
+
+					if pcount == TS_BATCH_SIZE {
+						tsresp.DataFrame = dataFrame.Name
+						tsresp.Identifier = uuStr
+						ctx.response = tsresp
+						stage.output <- ctx
+						tsresp = &mortarpb.FetchResponse{}
+						pcount = 0
+					}
+				}
+			}
+			// any left over
+			if len(tsresp.Times) > 0 {
+				tsresp.DataFrame = dataFrame.Name
+				tsresp.Identifier = uuStr
+				ctx.response = tsresp
+				stage.output <- ctx
+			}
+
+		}
+	}
+
+	return nil
+}
